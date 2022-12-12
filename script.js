d23e8be7d3904f2f2687b08d1cb2df6d1dbfb35a
sina <srezaeihaddad@gmail.com>
2022-12-12 17:18:28 -0500

    Get Numbers on Display

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+const numbers = document.querySelectorAll('.digit')
+
+
 function add(a,b) {
     return a+b;
 }
@@ -14,3 +17,16 @@ function divide(a,b) {
     return a/b;
 }
 
+function operate(operator, a, b) {
+    return operator(a, b);
+}
+
+function display(e) {
+    const space = document.querySelector('.display');
+    let number = e.target.textContent
+    space.textContent = number;
+}
+
+numbers.forEach(number =>
+    number.addEventListener('click', display)
+)
