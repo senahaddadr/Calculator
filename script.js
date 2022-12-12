40d2157639adeee87a80a9c828cbf9c83357923b
sina <srezaeihaddad@gmail.com>
2022-12-12 18:20:04 -0500

    Add Backspace and AC Functions

--- script.js
+++ script.js
@@ -24,7 +24,25 @@ function operate(operator, a, b) {
 function display(e) {
     const space = document.querySelector('.display');
     let number = e.target.textContent
-    space.textContent = number;
+    if (e.target.id == 'back') {
+        let str = space.textContent;
+        if (str.length == 1) {
+            space.textContent = '0';
+        }
+        else {
+            space.textContent = str.substring(0, str.length - 1)
+        };
+
+    }
+    else if (e.target.id == 'ac') {
+        space.textContent = '0';
+    }
+    else {
+    if (space.textContent == 0) {
+        space.textContent = ""
+    }
+    space.textContent += number;
+    }
 }
 
 numbers.forEach(number =>
